c0c3df9bc68c2c3accca3e6b14b2c62f12d3d1a5
alexf388 <alexchunkwan.fung@gmail.com>
2020-12-24 14:54:37 -0500

    First commit. Home page and navbar looks decent, somewhat complete. Added background image. Fixed some issues with the navbar not appearing, and the logo of the navbar being partially cut off after clicked on. This was solved just by pointing the href to itself, so the website appears to refresh.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'app-component', component: AppComponent },
+  { path: '', redirectTo: '/app-component', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+@NgModule({
+    imports: [
+        MDBBootstrapModule.forRoot()
+    ]
+});
+*/
